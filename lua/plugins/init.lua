73b876fa111de9e3677f8d3bbf44db1f9250e1bb
zhaoyouya <1445089819@qq.com>
2024-04-11 18:12:22 +0800

    add vue treesitter

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -57,6 +57,7 @@ return {
         "markdown",
         "markdown_inline",
         "cmake",
+        "vue"
       },
     },
   },
@@ -76,7 +77,27 @@ return {
         theme_conf = { border = true },
         previewer = false,
       },
-    },
-    lazy = false
+    }
   },
+  -- {
+  --   "JoosepAlviste/nvim-ts-context-commentstring",
+  --   lazy = false
+  -- },
+  -- {
+  --   "numToStr/Comment.nvim",
+  --   keys = {
+  --     { "gcc", mode = "n", desc = "Comment toggle current line" },
+  --     { "gc", mode = { "n", "o" }, desc = "Comment toggle linewise" },
+  --     { "gc", mode = "x", desc = "Comment toggle linewise (visual)" },
+  --     { "gbc", mode = "n", desc = "Comment toggle current block" },
+  --     { "gb", mode = { "n", "o" }, desc = "Comment toggle blockwise" },
+  --     { "gb", mode = "x", desc = "Comment toggle blockwise (visual)" },
+  --   },
+  --   config = function(_, _)
+  --     require("Comment").setup {
+  --       pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+  --     }
+  --   end,
+  --   lazy = false
+  -- },
 }
