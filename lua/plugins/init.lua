62bc64d0e6d0e68edc0ee60f810ade25a01b0f38
zerlei <1445089819@qq.com>
2025-03-28 23:56:33 +0800

    feat: change `copilot.vim` to `copilot.lua` <tab> for accept a word <S-Tab> for accept all .

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -62,13 +62,76 @@ return {
         previewer = false,
       },
     },
+    -- {
+    --   "github/copilot.vim",
+    --   lazy = false,
+    --   config = function() end,
+    --   opts = {},
+    -- },
     {
-      "github/copilot.vim",
-      lazy = false,
-      config = function() end,
-      opts = {},
+      "zbirenbaum/copilot.lua",
+      cmd = "Copilot",
+      event = "InsertEnter",
+      config = function()
+        require("copilot").setup {
+          --
+          panel = {
+            enabled = false,
+            auto_refresh = false,
+            keymap = {
+              jump_prev = "[[",
+              jump_next = "]]",
+              accept = "<CR>",
+              refresh = "gr",
+              open = "<M-CR>",
+            },
+            layout = {
+              position = "bottom", -- | top | left | right | horizontal | vertical
+              ratio = 0.4,
+            },
+          },
+          suggestion = { -- Completion options for copilot
+            enabled = true,
+            auto_trigger = true,
+            hide_during_completion = true,
+            debounce = 75,
+            keymap = {
+              accept = "<S-Tab>",
+              accept_word = "<Tab>",
+              accept_line = false,
+              next = "<M-]>",
+              prev = "<M-[>",
+              dismiss = "<C-]>",
+            },
+          },
+          filetypes = {
+            yaml = false,
+            markdown = true,
+            help = false,
+            gitcommit = false,
+            gitrebase = false,
+            hgcommit = false,
+            svn = false,
+            cvs = false,
+            ["."] = false,
+          },
+          logger = {
+            file = vim.fn.stdpath "log" .. "/copilot-lua.log",
+            file_log_level = vim.log.levels.OFF,
+            print_log_level = vim.log.levels.WARN,
+            trace_lsp = "off", -- "off" | "messages" | "verbose"
+            trace_lsp_progress = false,
+            log_lsp_messages = false,
+          },
+          copilot_node_command = "node", -- Node.js version must be > 18.x
+          workspace_folders = {},
+          copilot_model = "", -- Current LSP default is gpt-35-turbo, supports gpt-4o-copilot
+          server_opts_overrides = {},
+        }
+      end,
     },
   },
+  -- for auto comment
   {
     "danymat/neogen",
     config = true,
